83ee1953c4763c167da082b0c674b661fe144a86
dwelch <dwelch@dwelch.com>
2013-12-30 23:26:00 -0500

    much work done on thread_hand4 cleanup required in this directory, simple waiteu now working in simulation

--- simulator/threads/thread_hand4.s
+++ simulator/threads/thread_hand4.s
@@ -19,9 +19,25 @@ _start:
     ldc r11,0x10B
     set ps[r11],r10     # set vector base (to match ram base)
 
+    #ldc r11,0xb
+    #get r10,ps[r11]
+    #ldap r11,RamSize
+    #ldw r11,r11[0x0]
+    #add r0,r10,r11
+
+    #set sp,r0
+    #extsp 0x21
+    #ldap r11,cp
+    #set cp,r11
+    #ldap r11,dp
+    #set dp,r11
+
     ldc  r11, 0x6       # the I/O ports need a clock
     setc res[r11], 0x8  # setci
     setc res[r11], 0xf  # setci
+
+    #ldap r11,kep_handler
+    #set kep,r11
 
     # define XS1_PORT_32A 0x200000
     ldc r8,0x2000
@@ -43,16 +59,32 @@ setup_resources:
 
     setc res[r3],0x2    # define XS1_SETC_IE_MODE_EVENT 0x2
 
-    #ldc r0,1
-
     clre
-    eeu res[r3]
-    waiteu
+    eeu res[r3]         # wake up on this resources event
+    waiteu              # wait for an event to happen, then go to the handler
 
 tmr_vec:
     ldc r10,0xABCD
     out res[r8],r10
-    freer res[r3]
-    ldc r0,0
-    clre
-    waiteu
+    freer res[r3]       # free the resource that got us here (get rid if the timer, so no more timer event pending)
+    clre                # no events pending
+    waiteu              # no events registered so this waits forever or until the max-cycles hits on the sim
+
+#    .align 0x40
+#kep_handler:
+#    clre
+#    waiteu
+
+#RamSize: .word 0x00010000
+#cp: .word 0x01010101
+#dp: .word 0x00000000
+
+# Any instance of _DoSyscall seems to make the simulator happy when
+# using waiteu.  I still dont know why waiteu matters and not kcall
+# or dcall
+#_DoSyscall: #.word 0x77C0A6D0
+#_DoSyscall:
+#    mkmsk r0,0x20
+#    retsp 0
+_DoSyscall:
+    bu _DoSyscall
